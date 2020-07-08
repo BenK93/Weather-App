89dbed0a8562b28ce69a6b1bb263641b16adb05e
BenK93 <benkoren23@gmail.com>
2020-07-08 14:06:14 +0300

    small updates

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ window.addEventListener('load', () => {
     let tempDegree = document.querySelector(".temperature-degree");
     let tempDecs = document.querySelector(".temperature-decs");
     let iconElement = document.querySelector(".icon1");
+    let tempMeasure = document.querySelector(".degree-section span");
+    let temperature = document.querySelector(".temperature");
     let api_key = `c41070bc5ddf47c55b7c23d7b4701846`;
     if (navigator.geolocation){
         navigator.geolocation.getCurrentPosition(pos =>{
@@ -25,13 +27,24 @@ window.addEventListener('load', () => {
                 var timeZone = country + ' / ' +data.name;
                 locationTimeZone.textContent = timeZone;
                 tempDecs.textContent = description;
-                tempDegree.textContent = temp;
+                let celsius = temp;
+                tempDegree.textContent = celsius;
                 setIcon(description, iconElement);
+                temperature.addEventListener("click", () => {
+                    tempMeasure.textContent = "C" == tempMeasure.textContent ? "F" : "C";
+                    var bool = (tempMeasure.textContent == "C") ? true : false;
+                    let fahrenheit = (temp * 9 / 5) + 32;
+                    tempDegree.textContent = (bool == true) ? celsius : fahrenheit.toFixed(2);
+                })
             });
 
         });
     }else {
         // not knowing the location 
+        console.log("happending");
+        locationTimeZone.textContent = "😩Please allow location!";
+        tempDecs.textContent = "Refresh the page and try again";
+
     }
     function setIcon(weatherCondition, iconID){
         const skycon = new Skycons({color: "white"});
